d53a8ce5fe717f920f689c1b3f0400b3ebdbd375
Fifi Art <44811907+fifiinart@users.noreply.github.com>
2019-04-17 17:32:48 -0400

    Create kill.js

---
+++ commands/util/kill.js
@@ -0,0 +1,23 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class killCommand extends Command {
+  constructor(client) {
+    super(client, {
+      ownerOnly: true,
+      name: 'kill',
+      memberName: 'kill',
+      aliases: ['exit', 'destroy'],
+      group: 'util',
+      description: 'Kills the bot.',
+      details: 'Responds with "Bot was killed", and kills bot',
+      examples: ['alpha kill', 'alpha exit']
+    })
+  }
+
+  run( msg ) {
+    msg.say(`Bot was killed by ${msg.author.tag}.`)
+    console.log(`Alpha Ten was killed by ${msg.author.tag}`);
+    msg.command.client.destroy();
+    process.exit(0);
+  }
+}
