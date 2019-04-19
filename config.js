e18ac6f8cbc029c19c09aabc6b40479bc8fd96e8
fifiinart <ferenxpain64@163.com>
2019-04-18 20:07:37 -0400

    Update config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+require('dotenv')
+  .config();
+module.exports = {
+  botToken: process.env.BOT_TOKEN,
+  embedColor: 0x00FFFF,
+  version: '1.0.1',
+  prefix: 'alpha ',
+  owner: '521817052470575105'
+}
